67c34af76dc223f830d146300e7387ed3742656d
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-05-18 16:39:59 +0200

    fixed order, snack 4 now working and complete

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -3,7 +3,7 @@ let userNumber = prompt("Inserisci qui il tuo numero A QUATTRO CIFRE.");
 if (userNumber.length === 4 && isNaN(userNumber) === false) {
     console.log(userNumber)
     console.log("Calcolando la somma...")
-    console.log(parseInt(userNumber.charAt(1)) + parseInt(userNumber.charAt(2)) + parseInt(userNumber.charAt(3)) + parseInt(userNumber.charAt(4)));
+    console.log(parseInt(userNumber.charAt(0)) + parseInt(userNumber.charAt(1)) + parseInt(userNumber.charAt(2)) + parseInt(userNumber.charAt(3)));
 } else {
     console.log("Input non valido!");
 };
