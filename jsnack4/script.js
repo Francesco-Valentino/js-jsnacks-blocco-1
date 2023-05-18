ed249166fc6ab29727e13fa7a989702cf7d355b6
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-05-18 16:28:46 +0200

    added else condition

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -1,8 +1,10 @@
 let userNumber = prompt("Inserisci qui il tuo numero A QUATTRO CIFRE.");
 
-if (userNumber.length === 4 && isNaN(userNumber) === false){
+if (userNumber.length === 4 && isNaN(userNumber) === false) {
     console.log(userNumber)
     console.log("Calcolando la somma...")
+} else {
+    console.log("Input non valido!")
 }
 
 
