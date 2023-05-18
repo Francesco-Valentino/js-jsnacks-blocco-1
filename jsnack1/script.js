cd8bb2d4ebdd28e3d3c021fec90871864f1e26fe
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-05-18 15:15:00 +0200

    snack 1 working

---
+++ jsnack1/script.js
@@ -0,0 +1,32 @@
+const firstNum = parseInt(prompt("Insert first number"));
+
+const secondNum = parseInt(prompt("Insert second number"));
+
+const thirdNum = parseInt(prompt("Insert third number"));
+
+const fourthNum = parseInt(prompt("Insert fourth number"));
+
+const fifthNum = parseInt(prompt("Insert fifth number"));
+
+const sixthNum = parseInt(prompt("Insert sixth number"));
+
+const seventhNum = parseInt(prompt("Insert seventh number"));
+
+const eighthNum = parseInt(prompt("Insert eighth number"));
+
+const ninthNum = parseInt(prompt("Insert ninth number"));
+
+const tenthNum = parseInt(prompt("Insert tenth number"));
+
+console.log(firstNum + secondNum + thirdNum + fourthNum + fifthNum + sixthNum + seventhNum + eighthNum + ninthNum + tenthNum);
+
+//for (let i = 0; i < 10; i++){
+    //prompt("Type your number here");
+    //const mySum = ??? 
+//};
+//Non mi viene in mente un modo per aggiungere il valore espresso in un'iterazione del contatore al suo successivo  e per il successivo la somma dei due valori precedenti. Il programma funziona, ma non è ottimizzato.
+
+
+
+
+
