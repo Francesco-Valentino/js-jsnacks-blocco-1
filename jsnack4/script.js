6e49986d87ff81a0885e2199d4af219a2c68e133
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-05-18 16:37:35 +0200

    trying to use charAt() to extract each digit

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -3,9 +3,10 @@ let userNumber = prompt("Inserisci qui il tuo numero A QUATTRO CIFRE.");
 if (userNumber.length === 4 && isNaN(userNumber) === false) {
     console.log(userNumber)
     console.log("Calcolando la somma...")
+    console.log(parseInt(userNumber.charAt(1)) + parseInt(userNumber.charAt(2)) + parseInt(userNumber.charAt(3)) + parseInt(userNumber.charAt(4)));
 } else {
-    console.log("Input non valido!")
-}
+    console.log("Input non valido!");
+};
 
 
 
