f69f3523a1197fb316b7d98de5cb6ee23ad6d7c6
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-05-18 16:25:10 +0200

    building snack 4 software

---
+++ jsnack4/script.js
@@ -0,0 +1,9 @@
+let userNumber = prompt("Inserisci qui il tuo numero A QUATTRO CIFRE.");
+
+if (userNumber.length === 4 && isNaN(userNumber) === false){
+    console.log(userNumber)
+    console.log("Calcolando la somma...")
+}
+
+
+
